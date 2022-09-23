7f10fe4429b5060946500a6b0b179053266811da
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-09-23 21:52:53 +0900

    録音して再生する

---
+++ index.js
@@ -0,0 +1,42 @@
+const buttonStart = document.getElementById('button_start');
+const buttonStop = document.getElementById('button_stop');
+// const audio = document.getElementById('recorded_audio');
+
+/**
+ * @type {MediaRecorder?}
+ */
+let mediaRecorder = null;
+
+/**
+ * @type {Array<Blob>}
+ */
+let chunks = [];
+
+buttonStart.onclick = function () {
+    // マイク権限のリクエスト
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        .then((mediaStream) => {
+            console.log("allowed");
+            mediaRecorder = new MediaRecorder(mediaStream);
+            // データが来たら配列に突っ込んでいく
+            mediaRecorder.ondataavailable = function (ev) {
+                console.log("on data availabel");
+                chunks.push(ev.data);
+            };
+            // 録音を停止したら再生する
+            mediaRecorder.onstop = function (ev) {
+                const blob = new Blob(chunks);
+                const audioUrl = URL.createObjectURL(blob);
+                const audio = new Audio(audioUrl);
+                audio.play();
+            };
+            // 録音開始
+            mediaRecorder.start();
+        });
+}
+
+buttonStop.onclick = function () {
+    if (mediaRecorder != null) {
+        mediaRecorder.stop();
+    }
+}

4419e62228cb68d0cb8f1929e4a968ccccc1a087
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-09-24 12:04:47 +0900

    爆音処理したものをダウンロードする

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ window.onload = (event) => {
     const buttonStop = document.getElementById('button_stop');
 
     let recorder = new Tone.Recorder();
+    let explotionRecorder = new Tone.Recorder();
     let mic = null;
 
     buttonStart.onclick = function () {
@@ -34,7 +35,9 @@ window.onload = (event) => {
             recorder.stop().then((blob) => {
                 console.log("record stopped!!");
                 const blobUrl = URL.createObjectURL(blob);
-                const player = new Tone.Player(blobUrl);
+                const player = new Tone.Player(blobUrl, () => {
+                    explotionRecorder.start();
+                });
                 // 処理時間の都合上、ある程度で録音を止めてしまったほうが良いかもしれない
                 // 任意の秒数に変換できるように再生時間から何倍速にするか逆算したほうが良いかもしれない
                 const playbackRate = 3.0; // 何倍速にする？
@@ -54,8 +57,21 @@ window.onload = (event) => {
                 pitchDownNode.connect(reverbNode);
                 reverbNode.connect(limiterNode);
                 limiterNode.toDestination();
+                limiterNode.connect(explotionRecorder);
 
                 player.autostart = true;
+                player.onstop = function () {
+                    // 再生が終わってもリバーブの余韻があるので2秒くらい待つ
+                    setTimeout(() => {
+                        explotionRecorder.stop().then((blob) => {
+                            const url = URL.createObjectURL(blob);
+                            const anchor = document.createElement("a");
+                            anchor.download = "recording.webm";
+                            anchor.href = url;
+                            anchor.click();
+                        });
+                    }, 2000);
+                }
             });
 
         }

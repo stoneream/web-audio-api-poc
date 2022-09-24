f5b4c47f6b9caad051e15589dffee3b0bb43cb1d
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-09-24 10:07:04 +0900

    ToneJS版の録音と録音した音声の再生

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const buttonStart = document.getElementById('button_start');
-const buttonStop = document.getElementById('button_stop');
+window.onload = (event) => {
 
-let mic = null;
+    const buttonStart = document.getElementById('button_start');
+    const buttonStop = document.getElementById('button_stop');
 
-buttonStart.onclick = function () {
-    // マイク権限のリクエスト
-    mic = new Tone.UserMedia();
+    let recorder = new Tone.Recorder();
+    let mic = null;
 
-    const micFFT = new Tone.FFT();
+    buttonStart.onclick = function () {
+        Tone.context.resume();
 
-    mic.connect(micFFT);
+        // マイク権限のリクエスト
+        mic = new Tone.UserMedia();
 
-    // 録音開始
-    mic.open().then(() => {
-        console.log("recording....");
-    });
-}
+        // 録音データの流し先
+        mic.connect(recorder);
 
-buttonStop.onclick = function () {
-    if (mic != null) {
-        mic.close();
+        // 録音開始
+        mic.open().then(() => {
+            recorder.start().then(() => {
+                console.log("recording....");
+            });
+        });
+    }
+
+    buttonStop.onclick = function () {
+        Tone.context.resume();
+
+        if (mic != null) {
+            mic.close();
+
+
+            // 録音を止めて音声の再生を開始する
+            recorder.stop().then((blob) => {
+                console.log("record stopped!!");
+                const blobUrl = URL.createObjectURL(blob);
+                const player = new Tone.Player(blobUrl).toDestination();
+                player.autostart = true;
+            });
+
+        }
     }
 }

ffdae5e518e989fa2feb03b99c762a4a2e93a764
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-09-24 13:05:03 +0900

    色々ミスった

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ window.onload = (event) => {
     const buttonStart = document.getElementById('button_start');
     const buttonStop = document.getElementById('button_stop');
 
-    let recorder = new Tone.Recorder();
+    // とりあえず音声ファイルの長さは5秒で統一
+    const audioLengthSeconds = 5;
+
+    let micRecorder = new Tone.Recorder();
     let explotionRecorder = new Tone.Recorder();
     let mic = null;
 
@@ -14,11 +17,11 @@ window.onload = (event) => {
         mic = new Tone.UserMedia();
 
         // 録音データの流し先
-        mic.connect(recorder);
+        mic.connect(micRecorder);
 
         // 録音開始
         mic.open().then(() => {
-            recorder.start().then(() => {
+            micRecorder.start().then(() => {
                 console.log("recording....");
             });
         });
@@ -30,50 +33,50 @@ window.onload = (event) => {
         if (mic != null) {
             mic.close();
 
+            // 録音を停止して爆音処理をする
+            micRecorder.stop().then((blob) => {
+                Tone.Offline(() => {
+                    const blobUrl = URL.createObjectURL(blob);
+                    const player = new Tone.Player(blobUrl);
+                    player.connect(explotionRecorder);
 
-            // 録音を止めて音声の再生を開始する
-            recorder.stop().then((blob) => {
-                console.log("record stopped!!");
-                const blobUrl = URL.createObjectURL(blob);
-                const player = new Tone.Player(blobUrl, () => {
-                    explotionRecorder.start();
-                });
-                // 処理時間の都合上、ある程度で録音を止めてしまったほうが良いかもしれない
-                // 任意の秒数に変換できるように再生時間から何倍速にするか逆算したほうが良いかもしれない
-                const playbackRate = 3.0; // 何倍速にする？
-                player.playbackRate = playbackRate;
+                    player.onload = function () {
+                        explotionRecorder.start().then(() => {
+                            // 音声ファイルが指定の秒数になるように再生速度を調整する
+                            // 何倍速 = 音声の長さ / 指定の秒数
+                            const playbackRate = player.sampleTime / audioLengthSeconds;
 
-                // 音量をめちゃくちゃ上げる
-                const upGainNode = new Tone.Gain({ gain: 1024, convert: true });
-                // (倍速速度 + 1) * -12 くらいがちょうどいい
-                const pitchDownNode = new Tone.PitchShift({ pitch: -12 * playbackRate + 1.0, wet: 1.0 });
-                const reverbNode = new Tone.Reverb();
-                // スピーカーが壊れないようにリミッターをかませる
-                const limiterNode = new Tone.Limiter(-10.0);
+                            // 音量をめちゃくちゃ上げる
+                            const upGainNode = new Tone.Gain({ gain: 1024, convert: true });
+                            // (倍速速度 + 1) * -12 くらいがちょうどいい
+                            const pitchDownNode = new Tone.PitchShift({ pitch: -12 * playbackRate + 1.0, wet: 1.0 });
+                            const reverbNode = new Tone.Reverb();
+                            // スピーカーが壊れないようにリミッターをかませる
+                            const limiterNode = new Tone.Limiter(-10.0);
 
+                            player.connect(upGainNode);
+                            upGainNode.connect(pitchDownNode);
+                            pitchDownNode.connect(reverbNode);
+                            reverbNode.connect(limiterNode);
+                            limiterNode.toDestination();
 
-                player.connect(upGainNode);
-                upGainNode.connect(pitchDownNode);
-                pitchDownNode.connect(reverbNode);
-                reverbNode.connect(limiterNode);
-                limiterNode.toDestination();
-                limiterNode.connect(explotionRecorder);
-
-                player.autostart = true;
-                player.onstop = function () {
-                    // 再生が終わってもリバーブの余韻があるので2秒くらい待つ
-                    setTimeout(() => {
-                        explotionRecorder.stop().then((blob) => {
-                            const url = URL.createObjectURL(blob);
-                            const anchor = document.createElement("a");
-                            anchor.download = "recording.webm";
-                            anchor.href = url;
-                            anchor.click();
+                            player.start();
                         });
-                    }, 2000);
-                }
-            });
+                    }
+                    // リバーブがあるので+2秒くらい
+                }, audioLengthSeconds + 2).then((buffer) => {
+                    const player = new Tone.Convolver(buffer, () => {
+                        player.toDestination();
+                    });
+                    player.autostart = true;
+                    // const url = URL.createObjectURL(blob);
+                    // const anchor = document.createElement("a");
+                    // anchor.download = "recording.webm";
+                    // anchor.href = url;
+                    // anchor.click();
 
+                });
+            });
         }
     }
 }
